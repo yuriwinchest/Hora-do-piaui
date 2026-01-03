db294f0d151bde1a22506a2aea844fa25832e3b2
yuriwinchest <yuriallmeida@gmail.com>
2026-01-03 20:44:13 -0300

    Further tune main feature image height (aspect-7/8) for alignment

--- App.tsx
+++ App.tsx
@@ -212,7 +212,7 @@ const HomePage: React.FC = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-5 mb-12 items-stretch">
         <div className="lg:col-span-5 h-full">
-          <NewsCard item={MIDDLE_FEATURE} variant="vertical" imageClassName="aspect-[8/7]" />
+          <NewsCard item={MIDDLE_FEATURE} variant="vertical" imageClassName="aspect-[7/8]" />
         </div>
         <div className="lg:col-span-7 h-full flex flex-col divide-y divide-gray-100">
           {homeMiddleList.slice(0, 4).map((item) => (
