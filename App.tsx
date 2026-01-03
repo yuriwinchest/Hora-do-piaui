494b74f53d0788d34144bee223832b2222cbb04b
yuriwinchest <yuriallmeida@gmail.com>
2026-01-03 20:38:36 -0300

    Change main feature image to square aspect ratio for alignment

--- App.tsx
+++ App.tsx
@@ -212,7 +212,7 @@ const HomePage: React.FC = () => {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-5 mb-12 items-stretch">
         <div className="lg:col-span-5 h-full">
-          <NewsCard item={MIDDLE_FEATURE} variant="vertical" imageClassName="aspect-square md:aspect-[4/3]" />
+          <NewsCard item={MIDDLE_FEATURE} variant="vertical" imageClassName="aspect-square" />
         </div>
         <div className="lg:col-span-7 h-full flex flex-col divide-y divide-gray-100">
           {homeMiddleList.slice(0, 4).map((item) => (
